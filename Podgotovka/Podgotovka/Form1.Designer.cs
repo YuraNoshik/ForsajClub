7695a66513d6d4a91b0623cad4d19f19566fb3ab
yuran <yuranoshik@mail.ru>
2023-04-14 02:49:30 +0300

    session1

---
+++ Podgotovka/Podgotovka/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿
+namespace Podgotovka
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.CbFilterPrice = new System.Windows.Forms.ComboBox();
+            this.CbSort = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Column1 = new System.Windows.Forms.DataGridViewImageColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.DisplayedCells;
+            this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.DisplayedCells;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3});
+            this.dataGridView1.Location = new System.Drawing.Point(35, 89);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.Size = new System.Drawing.Size(734, 338);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // CbFilterPrice
+            // 
+            this.CbFilterPrice.FormattingEnabled = true;
+            this.CbFilterPrice.Location = new System.Drawing.Point(55, 50);
+            this.CbFilterPrice.Name = "CbFilterPrice";
+            this.CbFilterPrice.Size = new System.Drawing.Size(121, 21);
+            this.CbFilterPrice.TabIndex = 1;
+            this.CbFilterPrice.SelectedIndexChanged += new System.EventHandler(this.CbFilterPrice_SelectedIndexChanged);
+            // 
+            // CbSort
+            // 
+            this.CbSort.FormattingEnabled = true;
+            this.CbSort.Location = new System.Drawing.Point(196, 50);
+            this.CbSort.Name = "CbSort";
+            this.CbSort.Size = new System.Drawing.Size(121, 21);
+            this.CbSort.TabIndex = 2;
+            this.CbSort.SelectedIndexChanged += new System.EventHandler(this.CbSort_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(623, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "label1";
+            // 
+            // Column1
+            // 
+            this.Column1.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.DisplayedCells;
+            this.Column1.HeaderText = "Column1";
+            this.Column1.Name = "Column1";
+            this.Column1.Width = 54;
+            // 
+            // Column2
+            // 
+            this.Column2.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.Column2.DefaultCellStyle = dataGridViewCellStyle1;
+            this.Column2.HeaderText = "Column2";
+            this.Column2.Name = "Column2";
+            // 
+            // Column3
+            // 
+            this.Column3.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.DisplayedCells;
+            this.Column3.HeaderText = "Column3";
+            this.Column3.Name = "Column3";
+            this.Column3.Width = 73;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.CbSort);
+            this.Controls.Add(this.CbFilterPrice);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ComboBox CbFilterPrice;
+        private System.Windows.Forms.ComboBox CbSort;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridViewImageColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+    }
+}
+
---
+++ Podgotovka/Podgotovka/Form1.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Podgotovka;
+using Podgotovka.Entity;
+using Podgotovka.Properties;
+
+namespace Podgotovka
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            LoadPrice();
+            LoadSort();
+            FillGrid();
+            CountOfRows();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+   
+        }
+        private void LoadPrice()
+        {
+            CbFilterPrice.Items.Add("Все фильтры");
+            CbFilterPrice.Items.Add("1-15");
+            CbFilterPrice.Items.Add("15-30");
+            CbFilterPrice.Items.Add("30-50");
+            CbFilterPrice.SelectedItem = CbFilterPrice.Items[0];
+        }
+
+        private void LoadSort()
+        {
+            CbSort.Items.Add("Без сортировки");
+            CbSort.Items.Add("По возрастанию");
+            CbSort.Items.Add("По убыванию");
+            CbSort.SelectedItem = CbSort.Items[0];
+        }
+
+        private void CountOfRows()
+        {
+            using (ObuvEntities obuvEntities = new ObuvEntities())
+            {
+                label1.Text = $"{dataGridView1.Rows.Count.ToString()} из {obuvEntities.Product.Count()}";
+            }
+        }
+
+        Bitmap bitmap;
+        /// <summary>
+        /// Заполнение каталога
+        /// </summary>
+        private void FillGrid()
+        {
+            
+            string path = @"C:\Users\yuran\source\repos\Podgotovka\Podgotovka\Resources\";
+            dataGridView1.Rows.Clear();
+            using (ObuvEntities obuvEntities = new ObuvEntities())
+            {
+
+                var products = obuvEntities.Product.ToList();
+
+                switch (CbSort.SelectedIndex)
+                {
+                    default:
+                        break;
+                    case 1:
+                        products = products.OrderBy(x => x.productCost).ToList();
+                        break;
+                    case 2:
+                        products = products.OrderByDescending(x => x.productCost).ToList();
+                        break;
+                }
+
+                switch (CbFilterPrice.SelectedIndex)
+                {
+                    default:
+                        break;
+                    case 1:
+                        products = products.Where(x=> x.productActiveDiscountAmount >= 1 && x.productActiveDiscountAmount <= 15).ToList();
+                        break;
+                    case 2:
+                        products = products.Where(x => x.productActiveDiscountAmount >= 15 && x.productActiveDiscountAmount <= 30).ToList();
+                        break;
+                    case 3:
+                        products = products.Where(x => x.productActiveDiscountAmount >= 30 && x.productActiveDiscountAmount <= 50).ToList();
+                        break;
+                }
+       
+                for (int i = 0; i < products.Count; i++) 
+                {
+                    if (i != products.Count() - 1)
+                        dataGridView1.Rows.Add();
+
+                    byte? discount = products.Select(x=> x.productActiveDiscountAmount).ToArray()[i];
+
+                    if (!discount.Equals(null)) 
+                    {
+                        dataGridView1.Rows[i].Cells[1].Value =
+                            $"Название продукта {products.Select(x => x.productName).ToArray()[i]}\n" +
+                            $"Цена {Math.Round((decimal)products.Select(x => x.productCost).ToArray()[i], 0)}\n" +
+                            $"Скидка {products.Select(x => x.productActiveDiscountAmount).ToArray()[i]}\n";
+                            
+                        dataGridView1.Rows[i].Cells[2].Style.Font = new Font(dataGridView1.Font, FontStyle.Strikeout);
+                        dataGridView1.Rows[i].Cells[2].Value = $"Цена со скидкой {Math.Round((decimal)(products.Select(x => x.productCost).ToArray()[i] - products.Select(x => x.productCost).ToArray()[i] * products.Select(x => x.productActiveDiscountAmount).ToArray()[i] / 100), 0)} ";
+                    }
+                    else
+                    {
+                        dataGridView1.Rows[i].Cells[1].Value =
+                            $"Название продукта {products.Select(x => x.productName).ToArray()[i]}\n" +
+                            $"Цена {Math.Round((decimal)products.Select(x => x.productCost).ToArray()[i], 0)}\n";
+
+                    }
+                    if(products.Select(x=> x.productActiveDiscountAmount).ToArray()[i]>= 15)
+                    {
+                        dataGridView1.Rows[i].DefaultCellStyle.BackColor = Color.FromArgb(0x96, 0x81, 0x9B); 
+                    }
+
+                    var pic = products.Select(x=>x.productPicture).ToArray()[i];
+                    if(!String.IsNullOrEmpty(pic))
+                    {
+                        bitmap = new Bitmap(path + pic + ".jpg");
+                        bitmap = new Bitmap(bitmap, 128,128);
+                    }
+                    else
+                    {
+                        bitmap = new Bitmap(Resources.empty);
+                        bitmap = new Bitmap(bitmap, 128, 128);
+                    }
+                    dataGridView1.Rows[i].Cells[0].Value = bitmap;
+              
+                }
+            }
+
+            CountOfRows();
+
+        }
+
+        private void CbFilterPrice_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            FillGrid(); 
+        }
+
+        private void CbSort_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            FillGrid();
+        }
+    }
+}
